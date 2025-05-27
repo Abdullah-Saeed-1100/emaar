902fc1a2c0a3f676eb60675b4874d1e5eb64f62b
Abdullah Bagar <abdullah.saeed.bagar@gmail.com>
2025-05-27 09:38:35 +0300

    add flutter_localizations [ arabic only ]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const EmaarApp());
@@ -12,6 +13,16 @@ class EmaarApp extends StatelessWidget {
     return MaterialApp(
       title: 'Emaar App',
       theme: ThemeData(primarySwatch: Colors.blue),
+      // for Localization
+      locale: const Locale('ar'),
+      supportedLocales: const [
+        Locale('ar'), // العربية فقط حالياً
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       home: Scaffold(
         appBar: AppBar(title: const Text('Emaar App')),
         body: const Center(child: Text('Welcome to Emaar App!')),
